0d33fc58e0309916bbcf682bd7977f5c071e798b
itrajudevops <itrajuaws@gmail.com>
2023-05-10 13:24:32 +0530

    Terraform

--- manualservers.tf
+++ manualservers.tf
@@ -1,13 +1,3 @@
-data "aws_ami" "centos"{
-  most_recent      = true
-  name_regex       = "centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
-
-output "ami" {
-  value = data.aws_ami.centos.image_id
-}
-
 resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
