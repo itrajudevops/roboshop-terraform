737c44187808cb347acc26a7acabddb82bb25d16
itrajudevops <itrajuaws@gmail.com>
2023-05-10 13:19:00 +0530

    Terraform

--- manualservers.tf
+++ manualservers.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "centos" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "centos-8-DevOps-Practice"
+  most_recent      = true
+  name_regex       = "centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
 }
 
 output "ami" {
